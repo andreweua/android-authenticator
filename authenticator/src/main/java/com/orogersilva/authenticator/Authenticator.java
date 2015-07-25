53150a19c5f083062eeff95b8d3ab34c632ddb74
Roger Silva <orogersilva@gmail.com>
2015-07-25 19:35:24 -0300

    Refatoração do Authenticator

--- authenticator/src/main/java/com/orogersilva/authenticator/Authenticator.java
+++ authenticator/src/main/java/com/orogersilva/authenticator/Authenticator.java
@@ -7,20 +7,30 @@ public class Authenticator {
 
     // region INSTANCE VARIABLES
 
-    private final String address = "";
-
+    private String address;
     private String username;
     private String password;
 
     // endregion
 
-    public Authenticator(String username, String password) {
+    public Authenticator(String address, String username, String password) {
 
+        setAddress(address);
         setUsername(username);
         setPassword(password);
     }
 
     // region GETTERS AND SETTERS
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public void setAddress(String address) {
+
+        this.address = address;
+    }
 
     public String getUsername() {
 
